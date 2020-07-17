92a34f7204a0514913a6a5926b54ddd1d1834b6e
Scott H Lee <slee@logicfirstllc.com>
2020-07-17 18:27:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,7 +2,10 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-  "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + "s" + string
+  10.times do
+    ses += "s"
+  end
+  ses + string
     
   else
     string
